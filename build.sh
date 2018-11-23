a1e5b1c2bb345c3dfe84624e7e976fb1feba56f8
Edwin Kofler <24364012+EanKeen@users.noreply.github.com>
2018-11-23 13:45:11 -0800

    Create build script

--- build.sh
+++ build.sh
@@ -1,2 +1,10 @@
-#!/bin/bash
+if [[ ! -d ./node_modules ]]; then
+  echo "[build.sh] Installing npm Modules"
+	    npm install
+else
+	  echo
+fi
 
+wait
+echo "[build.sh] Creating dev server at port 8080"
+npm run serve
