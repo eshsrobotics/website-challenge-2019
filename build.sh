dc041f69c7a1604c31194a309b46e459d2e3731d
Edwin Kofler <24364012+EanKeen@users.noreply.github.com>
2018-12-15 13:43:02 -0800

    Comment build.sh

--- build.sh
+++ build.sh
@@ -10,15 +10,14 @@ currentBranch="$(git rev-parse --abbrev-ref HEAD)"
 # Stop if on master
 if [[ "$currentBranch" != "gh-pages" ]]; then
 	echo "Build complete; access website in ./dist/index.html"
-	echo "$currentBranch"
 else
-	# Erase dir contents
-	# folders
+	# Remove directories
 	find . -mindepth 1 -maxdepth 1 -type d -not -ipath "./dist" -and -not  -ipath "./.git" -exec echo rm -vf {} ";"
 
-	# erase files
-	find . -maxdepth 1 -type f -and -not -ipath "*build.sh" -exec echo rm - vf {} ";"
+	# Remove files
+	find . -maxdepth 1 -type f -and -not -ipath "*build.sh" -exec echo rm -vf {} ";"
 
+	# Move contnts of dist to parent folder
 	echo mv -vf ./dist/* ./
 	echo rmdir -vf ./dist
 fi
