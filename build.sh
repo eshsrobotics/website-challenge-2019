c6a2f5ee835334d87026547f8b62c65be3e172bd
Edwin Kofler <24364012+EanKeen@users.noreply.github.com>
2018-12-15 14:13:45 -0800

    Update build script

--- build.sh
+++ build.sh
@@ -1,8 +1,4 @@
 #!/usr/bin/env bash
-
-# We assume we're on the gh-pages release branch
-
-## 1 ##
 # Build release and place (generates in dist folder)
 # npm run build
 
@@ -12,12 +8,12 @@ if [[ "$currentBranch" != "gh-pages" ]]; then
 	echo "Build complete; access website in ./dist/index.html"
 else
 	# Remove directories
-	find . -mindepth 1 -maxdepth 1 -type d -not -ipath "./dist" -and -not  -ipath "./.git" -exec echo rm -vf {} ";"
+	find . -mindepth 1 -maxdepth 1 -type d -not -ipath "./dist" -and -not  -ipath "./.git" -exec rm -vrf {} ";"
 
 	# Remove files
-	find . -maxdepth 1 -type f -and -not -ipath "*build.sh" -exec echo rm -vf {} ";"
+	find . -maxdepth 1 -type f -and -not -ipath "*build.sh" -exec rm -vf {} ";"
 
 	# Move contnts of dist to parent folder
-	echo mv -vf ./dist/* ./
-	echo rmdir -vf ./dist
+	mv -v ./dist/* ./
+	rmdir -v ./dist
 fi
