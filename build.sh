ef541ba5bb12766444ef0d8051605f014a462c4a
Edwin Kofler <24364012+EanKeen@users.noreply.github.com>
2018-12-15 13:03:19 -0800

    Created build script

--- build.sh
+++ build.sh
@@ -1,8 +1,24 @@
-if [[ ! -d ./node_modules ]]; then
-  echo "[build.sh] Installing npm Modules"
-	npm install
-	wait
-fi
+#!/usr/bin/env bash
 
-echo "[build.sh] Creating dev server at port 8080"
-npm run serve
+# We assume we're on the gh-pages release branch
+
+## 1 ##
+# Build release and place (generates in dist folder)
+npm run build
+
+local currentBranch="$(git rev-parse --abrev-ref HEAD)"
+# Stop if on master
+if [[ "$currentBranch" != "gh-pages" ]]; then
+	echo "Build complete; access website in ./dist/index.html"
+
+else
+	# Erase dir contents
+	# folders
+	find . -mindepth 1 -maxdepth 1 -type d -not -ipath "./dist" -and -not  -ipath "./.git" -exec echo rm -vf {} ";"
+
+	# erase files
+	find . -maxdepth 1 -type f -and -not -ipath "*build.sh" -exec echo rm - vf {} ";"
+
+	echo mv -vf ./dist/* ./
+	echo rmdir -vf ./dist
+fi
