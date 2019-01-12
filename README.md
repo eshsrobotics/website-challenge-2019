d854f1aa14077db01f19f2e287aea597bcaa98f5
EanKeen <24364012+EanKeen@users.noreply.github.com>
2019-01-11 23:31:21 -0800

    Make image viewing more adaptive to browser width

--- README.md
+++ README.md
@@ -32,7 +32,6 @@ $ ./deploy.sh
 * Theme support
 * Path transitions
 * Create abstraction over image element, such that clicking on image yields an enlarged photo, with extra features
-* Have descriptions of photographs on the back, via flip on hover
 * Fix picture layout breaks with portrait dimensioned picture
 
 ## Maintenance
--- src/components/card-layout/LayoutCardsStruct.vue
+++ src/components/card-layout/LayoutCardsStruct.vue
@@ -15,9 +15,18 @@ export default {
 
 .cards-struct {
   display: grid;
-  grid-template-columns: repeat(auto-fill, 325px); // TODO: Fix this temporary fix
+  grid-template-columns: repeat(auto-fill, minmax(250px, 1fr)); // TODO: Fix this temporary fix
   grid-template-rows: repeat(auto-fill, 1fr);
-  grid-gap: 15px;
+  /*!*grid-auto-flow: dense;*!*/
+  /*display: grid;*/
+  /*grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));*/
+  /*grid-auto-rows: 200px;*/
+  /*grid-auto-flow: dense;*/
+  grid-gap: 1em;
+}
+
+.cards-struct * {
+  display: inline-block;
 }
 </style>
 
