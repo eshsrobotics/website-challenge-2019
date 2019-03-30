9f5e9225f578b42ec87c3d27511f4d4cff0ea869
Eankeen <24364012+EanKeen@users.noreply.github.com>
2019-03-29 23:08:57 -0700

    Replace footer social media facebook link to github

--- src/components/footer/Foot.vue
+++ src/components/footer/Foot.vue
@@ -25,9 +25,9 @@
           </a>
         </li>
         <li>
-          <a href="https://www.facebook.com/eshspotatoes" target="_blank">
-            <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="#343a40" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-facebook"><path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z"></path></svg>
-            <p>Facebook</p>
+          <a href="https://www.github.com/eshsrobotics" target="_blank">
+            <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"> <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22" /></svg>
+            <p>GitHub</p>
           </a>
         </li>
       </ul>
